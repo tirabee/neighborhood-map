2cfec1fd4ea63b9f0a9e6b80a9104374d03f9439
Sarah Rusch <sarah@papersapphire.com>
2018-10-01 20:52:56 -0600

    map working, initial location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import MapContainer from "./MapContainer";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-  <Map />
+        <MapContainer />
       </div>
     );
   }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,21 +1,30 @@
+import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        initialCenter={{
+          lat: 40.409934,
+          lng: -104.729065
+        }}
+      >
+        <Marker
+          name={"Woodbriar Park"}
+          title={'Woodbriar Park'}
+          position={{
+            lat: 40.409934,
+            lng: -104.729065
+          }}
+        />
       </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: AIzaSyAoGpvMXxTawpEiDCrR95JZBiWcc1eYZt0
+  apiKey: "AIzaSyAoGpvMXxTawpEiDCrR95JZBiWcc1eYZt0"
 })(MapContainer);
